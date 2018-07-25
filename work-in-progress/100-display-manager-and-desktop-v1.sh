82304c816f03ec0bb7be363e85abd47c2e3c266a
Neil Houghton <n.houghton@blueyonder.co.uk>
2018-07-25 09:44:52 +0100

    testing script

--- work-in-progress/100-display-manager-and-desktop-v1.sh
+++ work-in-progress/100-display-manager-and-desktop-v1.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 set -e
-##################################################################################################################
-# Author	:	Erik Dubois
-# Website	:	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxforum.com
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
-##################################################################################################################
+################################################################################
+#                                                                              #
+#                              RUN AT YOUR OWN RISK.                           #
+#                                                                              #
+################################################################################
 echo "#################################################"
 echo "If it feels like the downloads are too slow"
 echo "Stop the installation with CTRL + C"
@@ -20,9 +13,10 @@ echo "#################################################"
 
 sudo pacman -Syyu
 sudo pacman -S lightdm --noconfirm --needed
+sudo pacman -S lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed
 sudo pacman -S openbox --noconfirm --needed
-sudo systemctl enable lightdm.service -f
-sudo systemctl set-default graphical.target
+sudo systemctl enable lightdm.service
+#sudo systemctl set-default graphical.target
 
 
 #Remove anything you do not like from the installed applications
