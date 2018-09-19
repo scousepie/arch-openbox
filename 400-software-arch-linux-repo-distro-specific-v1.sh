48fa4334c3f2da270df8fc7ad2aa554b14c5940e
Neil <n.houghton@blueyonder.co.uk>
2018-09-19 17:10:29 +0100

    added gnome-themes-extra

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -10,6 +10,7 @@ set -e
 
 sudo pacman -S --noconfirm --needed compton
 sudo pacman -S --noconfirm --needed ffmpegthumbnailer
+sudo pacman -S --noconfirm --needed gnome-themes-extra
 sudo pacman -S --noconfirm --needed gsimplecal
 sudo pacman -S --noconfirm --needed gvfs
 sudo pacman -S --noconfirm --needed libopenraw
